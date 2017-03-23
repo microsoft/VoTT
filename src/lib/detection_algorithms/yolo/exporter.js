6b54aff06be85cddbdf7cd63e10497fd5e9d75ae
Nadav Bar <nabar@microsoft.com>
2017-03-23 11:01:45 +0200

    Added working YOLO exporter and test

--- src/lib/detection_algorithms/yolo/exporter.js
+++ src/lib/detection_algorithms/yolo/exporter.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-const DEFAULT_DATA_SET_NAME = "obj"
+const DEFAULT_DATA_SET_NAME = 'obj';
 const CFG_TEMPLATE_FILE_PATH = path.join(__dirname, 'yolo-obj.cfg.template');
 
 const OBJ_DATA_TAMPLATE = 'classes = %s\n' + 
                           'train  = train.txt\n' +
                           'valid  = test.txt\n' +
-                          'names = %s.names' +
+                          'names = %s.names\n' +
                           'backup = backup/'
 
 
@@ -27,16 +27,26 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
     self.exportDirPath = exportDirPath;
     self.dataDirPath = path.join(self.exportDirPath, 'data');
     self.imagesDirPath = path.join(self.dataDirPath, self.dataSetName);
+    self.trainFilePath = path.join(self.dataDirPath, 'train.txt');
     self.classes = classes;
     self.frameWidth = frameWidth;
-    self.frameHeight;
+    self.frameHeight = frameHeight;
     self.cfgTemplate = null;
+
+    // will be used later when writing the image data to file
+    self.classesDict = {};
+    for (var i in classes) {
+        self.classesDict[classes[i]] = i;
+    }
+
     // Prepare everything for exporting (e.g. create metadata files,
     // directories, ..)    
     // Returns: A Promise object that resolves when the operation completes
     this.init = function init() {
         return new Promise(function(resolve, reject) {
             async.waterfall([
+                ensureDirExists.bind(null, self.exportDirPath),
+                deleteFileIfExists.bind(null, self.trainFilePath),
                 function getTemplate(cb) {
                     if (self.cfgTemplate) {
                         return cb();
@@ -45,7 +55,7 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
                         if (err) {
                             return cb(err)
                         }
-                        self.cfgTemplate = data;
+                        self.cfgTemplate = data.toString();
                         cb();
                     })
                 },
@@ -59,7 +69,7 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
                 function saveObjectNames(cb) {
                     var objectNames = '';
                     for (var i in self.classes) {
-                        objectNames += self.classes[i] + "\n";
+                        objectNames += self.classes[i] + '\n';
                     }
                     fs.writeFile(path.join(self.dataDirPath, self.dataSetName + '.names'), objectNames, cb)
                 },
@@ -89,29 +99,6 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
     //         of the bounding boxes (respectively), and 'class' is the name of the class.
     // Returns: A Promise object that resolves when the operation completes
     this.exportFrame = function exportFrame(frameFileName, frameBuffer, tags) {
-
-        /*
-        Put image-files (.jpg) of your objects in the directory build\darknet\x64\data\obj\
-
-            Create .txt-file for each .jpg-image-file - in the same directory and with the same name, but with .txt-extension, and put to file: object number and object coordinates on this image, for each object in new line: <object-class> <x> <y> <width> <height>
-
-            Where:
-
-            <object-class> - integer number of object from 0 to (classes-1)
-            <x> <y> <width> <height> - float values relative to width and height of image, it can be equal from 0.0 to 1.0
-            for example: <x> = <absolute_x> / <image_width> or <height> = <absolute_height> / <image_height>
-            atention: <x> <y> - are center of rectangle (are not top-left corner)
-            For example for img1.jpg you should create img1.txt containing:
-
-            1 0.716797 0.395833 0.216406 0.147222
-            0 0.687109 0.379167 0.255469 0.158333
-            1 0.420312 0.395833 0.140625 0.166667
-            Create file train.txt in directory build\darknet\x64\data\, with filenames of your images, each filename in new line, with path relative to darknet.exe, for example containing:
-            data/obj/img1.jpg
-            data/obj/img2.jpg
-            data/obj/img3.jpg
-        */
-
         return new Promise(function(resolve, reject) {
             async.waterfall([
                 ensureDirExists.bind(null, self.imagesDirPath),
@@ -120,12 +107,24 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
                     fs.writeFile(imageFilePath, frameBuffer, cb); 
                 },
                 function saveBBoxesData(cb) {
-                    imageDataFilePath = path.join(self.imagesDirPath, frameFileName + ".txt");
+                    imageDataFilePath = path.join(self.imagesDirPath, frameFileName + '.txt');
                     var bboxesData = '';
-                    for 
+                    for (var i in tags) {
+                        var tag = tags[i];
+                        var classIndex = self.classesDict[tag.class];
+                        var relWidth = (tag.x2 - tag.x1) / self.frameWidth;
+                        var relHeight = (tag.y2 - tag.y1) / self.frameHeight;
+                        var relCenterX = (tag.x1 / self.frameWidth) + (relWidth / 2);
+                        var relCenterY = (tag.y1 / self.frameHeight) + (relHeight / 2);
+
+                        bboxesData += util.format('%s %s %s %s %s\n', classIndex, relCenterX.toFixed(6), 
+                                               relCenterY.toFixed(6), relWidth.toFixed(6), relHeight.toFixed(6));
+                    }
+                    fs.writeFile(imageDataFilePath, bboxesData, cb);
                 },
                 function updateFilesList(cb) {
-
+                    var lineToAppend = 'data/' + self.dataSetName + '/' + frameFileName + '\n';
+                    fs.appendFile(self.trainFilePath, lineToAppend, cb);
                 }
             ],
             function(err) {
@@ -145,6 +144,18 @@ function Exporter(exportDirPath, classes, frameWidth, frameHeight) {
             if (err) {
                 if (err.code == 'EEXIST') { 
                     return cb(); // ignore the error if the folder already exists
+                }
+                return cb(err);
+            }
+            cb();
+        });
+    }
+
+    function deleteFileIfExists(path, cb) {
+        fs.unlink(path, (err) => {
+            if (err) {
+                if (err.code == 'ENOENT') {
+                    return cb();
                 }
                 return cb(err);
             }
