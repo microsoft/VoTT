4f6d64436875606eb2d28c22721ea17cfd3be373
Tanner Barlow <tanner.barlow12@gmail.com>
2018-11-16 17:23:10 -0800

    move schemas to json files

--- src/components/pages/connectionsPage.tsx
+++ src/components/pages/connectionsPage.tsx
@@ -1,57 +1,8 @@
 import React from 'react';
 import Form from 'react-jsonschema-form'
 
-const schema = {
-    title: "Add Storage Connection",
-    required: ['displayName', "storageProvider", "connectionString", "containerName"],
-    type: "object",
-    properties: {
-        displayName: {
-            title: "Display Name",
-            type: "string"
-        },
-        storageProvider: {
-            title: "Provider",
-            type: "string",
-            enum: [
-                "azure-blob-storage",
-                "local"
-            ],
-            default: "azure-blob-storage",
-            enumNames: [
-                "Azure Blob Storage",
-                "Local Directory"
-            ]
-        },
-        connectionString: {
-            title: "Connection String",
-            type: "string"
-        },
-        containerName: {
-            title: "Container Name",
-            type: "string"
-        },
-        createContainer: {
-            title: "Create Container",
-            type: "boolean"
-        }
-    }
-}
-
-const uiSchema = {
-    displayName: {
-        "ui:autofocus": true,
-        "ui:placeholder": "My Data Connection"
-    },
-    connectionString: {
-        "ui:autofocus": true,
-        "ui:placeholder": "DefaultEndpointsProtocol=https;AccountName=myblobstorage;AccountKey=ABC123"
-    },
-    containerName: {
-        "ui:autofocus": true,
-        "ui:placeholder": "my-container"
-    }
-}
+const schema = require('./schemas/connectionsPage.json');
+const uiSchema = require('./schemas/ui/connectionsPage.json');
 
 const log = (type) => console.log.bind(console, type);
 
--- src/components/pages/projectSettingsPage.tsx
+++ src/components/pages/projectSettingsPage.tsx
@@ -1,63 +1,14 @@
 import React from 'react';
 import Form from 'react-jsonschema-form'
 
-const schema = {
-    title: "Project Settings",
-    type: "object",
-    properties: {
-        projectName: {
-            title: "Project Name",
-            type: "string"
-        },
-        dataSource: {
-            title: "Data Source",
-            type: "string",
-            enum: [
-                "my-connection-1",
-                "my-connection-2"
-            ],
-            enumNames: [
-                "My Connection 1",
-                "My Connection 2"
-            ]
-        },
-        // addConnectionButton: {
-        //     title: "Add connection",
-        //     type: "button"
-        // },
-        exportFormat: {
-            title: "Export Format",
-            type: "string",
-            enum: [
-                "tensorflow",
-                "cntk"
-            ],
-            enumNames: [
-                "Tensorflow",
-                "CNTK"
-            ]
-        },
-        description: {
-            title: "Description",
-            type: "string"
-        },
-        tags: {
-            title: "Tags",
-            type: "string"
-        },
-        imageSettings: {
-
-        },
-        videoSettings: {
-
-        }
-    }
-}
+const schema = require('./schemas/projectSettingsPage.json');
+const uiSchema = require('./schemas/ui/projectSettingsPage.json');
 
 export default class ProjectSettingsPage extends React.Component {
     render() {
         return (
-            <Form schema={schema} />
+            <Form schema={schema} 
+            uiSchema={uiSchema}/>
         );
     }
 }
