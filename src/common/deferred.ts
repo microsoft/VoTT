3f80289afdd82050765aed4eac474a6dfb9dffdd
Wallace Breza <wallace.breza@microsoft.com>
2018-11-19 11:27:27 -0800

    Added wrapper for ipc based promises

---
+++ src/common/deferred.ts
@@ -0,0 +1,25 @@
+export interface IDeferred<T> {
+    resolve(result: T): void;
+    reject(err: any): void;
+    then(value: T): Promise<T>
+    catch(err: any): Promise<T>
+}
+
+export class Deferred<T> implements IDeferred<T> {
+    promise: Promise<T>;
+
+    resolve = (result: T) => { };
+    reject = (err: any) => { };
+    then = (value: T) => { throw new Error("Not implemented yet"); };
+    catch = (err: any) => { throw new Error("Not implemented yet"); };
+
+    constructor() {
+        this.promise = new Promise<T>((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+
+        this.then = this.promise.then.bind(this.promise);
+        this.catch = this.promise.catch.bind(this.promise);
+    }
+}
---
+++ src/common/ipcRendererProxy.ts
@@ -0,0 +1,52 @@
+import { Deferred } from './deferred';
+import { IpcProxyMessage } from './ipcProxy';
+import * as shortid from 'shortid';
+
+export class IpcRendererProxy {
+    private static ipcRenderer;
+    private static initialized: boolean = false;
+    private static pending: { [id: string]: Deferred<any> } = {};
+
+    static initialize() {
+        if (IpcRendererProxy.initialized) {
+            return;
+        }
+
+        debugger;
+        IpcRendererProxy.ipcRenderer = (<any>window).require('electron').ipcRenderer;
+        IpcRendererProxy.ipcRenderer.on('ipc-renderer-proxy', (sender, message: IpcProxyMessage<any>) => {
+            const deferred = IpcRendererProxy.pending[message.id];
+            if (!deferred) {
+                throw new Error(`Cannot find deferred with id '${message.id}'`)
+            }
+
+            if (message.error) {
+                deferred.reject(message.error);
+            } else {
+                deferred.resolve(message.result);
+            }
+
+            delete IpcRendererProxy.pending[message.id];
+        });
+
+        IpcRendererProxy.initialized = true;
+    }
+
+    static send<TResult, TArgs>(type: string, args: TArgs = undefined): Promise<TResult> {
+        IpcRendererProxy.initialize();
+
+        const id = shortid.generate();
+        const deferred = new Deferred<TResult>();
+        IpcRendererProxy.pending[id] = deferred;
+
+        const outgoingArgs: IpcProxyMessage<TArgs> = {
+            id: id,
+            type: type,
+            args: args
+        };
+
+        IpcRendererProxy.ipcRenderer.send('ipc-main-proxy', outgoingArgs)
+
+        return deferred.promise;
+    }
+}
