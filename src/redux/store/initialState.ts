5acc4f99ccd859daefb53195bf0401a6c3bf084e
Jordan Ellis <joelli@microsoft.com>
2018-12-11 11:22:35 -0800

    removed unused test code

--- src/redux/store/initialState.ts
+++ src/redux/store/initialState.ts
@@ -1,8 +1,4 @@
-import shortid from "shortid";
-import { IConnection, IProject, ITag, IApplicationState } from "../../models/applicationState";
-
-const sampleConnections = createSampleConnections();
-const sampeProjects = createSampleProjects();
+import { IApplicationState } from "../../models/applicationState";
 
 const initialState: IApplicationState = {
     appSettings: {
@@ -13,57 +9,5 @@ const initialState: IApplicationState = {
     recentProjects: null, // sampeProjects,
     currentProject: null,
 };
-
-function createSampleConnections(count: number = 5): IConnection[] {
-    const connections: IConnection[] = [];
-    for (let i = 1; i <= count; i++) {
-        connections.push({
-            id: shortid.generate(),
-            name: `Connection ${i}`,
-            providerType: "sample",
-            providerOptions: {},
-        });
-    }
-
-    return connections;
-}
-
-function createSampleProjects(count: number = 5): IProject[] {
-    const projects: IProject[] = [];
-    for (let i = 1; i <= count; i++) {
-        projects.push({
-            id: shortid.generate(),
-            name: `Project ${i}`,
-            description: `Sample description for connection ${i}`,
-            exportFormat: {
-                providerType: "azureCustomVision",
-                providerOptions: {},
-            },
-            sourceConnection: getRandomConnection(sampleConnections),
-            targetConnection: getRandomConnection(sampleConnections),
-            tags: createSampleTags(),
-            autoSave: true,
-        });
-    }
-
-    return projects;
-}
-
-function getRandomConnection(connections: IConnection[]): IConnection {
-    const index = Math.floor((Math.random() * connections.length));
-    return connections[index];
-}
-
-function createSampleTags(count: number = 10): ITag[] {
-    const tags: ITag[] = [];
-    for (let i = 1; i <= count; i++) {
-        tags.push({
-            name: `tag-${i}`,
-            color: "#333",
-        });
-    }
-
-    return tags;
-}
 
 export default initialState;
