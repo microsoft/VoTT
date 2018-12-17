4216a37466122902a7cbf5cae150b0ecc15299d0
Wallace Breza <wallace.breza@microsoft.com>
2018-12-17 14:54:27 -0800

    Added unit tests for application actions

---
+++ src/redux/actions/applicationActions.test.ts
@@ -0,0 +1,39 @@
+import createMockStore, { MockStoreEnhanced } from "redux-mock-store";
+import { toggleDevToolsAction, refreshApplicationAction } from "./applicationActions";
+import { IpcRendererProxy } from "../../common/ipcRendererProxy";
+import { ActionTypes } from "./actionTypes";
+
+describe("Application Actions", () => {
+    let store: MockStoreEnhanced;
+
+    beforeEach(() => {
+        IpcRendererProxy.send = jest.fn(() => Promise.resolve());
+        store = createMockStore()();
+    });
+
+    it("Toggle Dev Tools action forwards call to IpcRenderer proxy", () => {
+        const action = toggleDevToolsAction(true);
+        expect(action.type).toEqual(ActionTypes.TOGGLE_DEV_TOOLS_SUCCESS);
+
+        store.dispatch(action);
+        const actions = store.getActions();
+
+        expect(actions.length).toEqual(1);
+        expect(actions[0]).toEqual(action);
+
+        expect(IpcRendererProxy.send).toBeCalledWith("TOGGLE_DEV_TOOLS", action.payload);
+    });
+
+    it("Reload application action forwards call to IpcRenderer proxy", () => {
+        const action = refreshApplicationAction();
+        expect(action.type).toEqual(ActionTypes.REFRESH_APP_SUCCESS);
+        
+        store.dispatch(action);
+        const actions = store.getActions();
+
+        expect(actions.length).toEqual(1);
+        expect(action[0]).toEqual(action);
+
+        expect(IpcRendererProxy.send).toBeCalledWith("RELOAD_APP");
+    });
+});
