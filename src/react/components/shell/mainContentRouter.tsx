28279a4f2b08ccc590ea97c7d54995fc232423bc
Luisa de Miranda <lumirand@microsoft.com>
2019-01-09 14:10:00 -0800

    much better way of handling default route; fixed linter error

--- src/react/components/shell/mainContentRouter.tsx
+++ src/react/components/shell/mainContentRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "../pages/homepage/homePage";
 import ActiveLearningPage from "../pages/activeLearningPage";
 import AppSettingsPage from "../pages/appSettings/appSettingsPage";
@@ -12,17 +12,19 @@ import ProfileSettingsPage from "../pages/profileSettingsPage";
 export default function MainContentRouter() {
     return (
         <div className="app-content">
-            {window.location.pathname.includes('index.html') && <Redirect to="/" />}
-            <Route path="/" exact component={HomePage} />
-            <Route path="/settings" component={AppSettingsPage} />
-            <Route path="/profile" component={ProfileSettingsPage} />
-            <Route path="/connections/:connectionId" component={ConnectionPage} />
-            <Route path="/connections" exact component={ConnectionPage} />
-            <Route path="/projects/:projectId/edit" component={EditorPage} />
-            <Route path="/projects/create" component={ProjectSettingsPage} />
-            <Route path="/projects/:projectId/settings" component={ProjectSettingsPage} />
-            <Route path="/projects/:projectId/export" component={ExportPage} />
-            <Route path="/projects/:projectId/active-learning" component={ActiveLearningPage} />
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/settings" component={AppSettingsPage} />
+                <Route path="/profile" component={ProfileSettingsPage} />
+                <Route path="/connections/:connectionId" component={ConnectionPage} />
+                <Route path="/connections" exact component={ConnectionPage} />
+                <Route path="/projects/:projectId/edit" component={EditorPage} />
+                <Route path="/projects/create" component={ProjectSettingsPage} />
+                <Route path="/projects/:projectId/settings" component={ProjectSettingsPage} />
+                <Route path="/projects/:projectId/export" component={ExportPage} />
+                <Route path="/projects/:projectId/active-learning" component={ActiveLearningPage} />
+                <Route component={HomePage} />
+            </Switch>
         </div>
     );
 }
