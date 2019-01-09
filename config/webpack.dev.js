fcbb8433bd421143702a2898744935c5ed34bfb4
JordanEllis6809 <joelli@microsoft.com>
2019-01-09 15:33:16 -0800

    removed uneeded changes

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,8 +4,4 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devtool: "inline-source-map",
-  node: {
-    __dirname: false,
-    __filename: false
-  }
 })
