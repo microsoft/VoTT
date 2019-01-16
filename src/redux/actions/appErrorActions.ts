968768d169c9ec531e1d952b600d4dbecfa9265e
My Ho <mydiemho@gmail.com>
2019-01-16 10:28:45 -0800

    add jsdoc

--- src/redux/actions/appErrorActions.ts
+++ src/redux/actions/appErrorActions.ts
@@ -3,30 +3,60 @@ import { IAppError } from "../../models/applicationState";
 import { createPayloadAction, IPayloadAction, createAction } from "./actionCreators";
 import { ActionTypes } from "./actionTypes";
 
+/**
+ * Action to display alert when there's an error in the app
+ * @member showError
+ * @memeber clearError
+ * @interface
+ */
 export default interface IAppErrorActions {
     showError(appError: IAppError): void;
     clearError(): void;
 }
 
+/**
+ * show alert popup to indicate error
+ * @param {IAppError} the error to display in alert
+ * @returns {(dispatch: Dispatch) => void}
+ */
 export function showError(appError: IAppError): (dispatch: Dispatch) => void {
     return (dispatch: Dispatch) => {
         dispatch(showErrorAction(appError));
     };
 }
 
+/**
+ * clear alert popup
+ * @returns {(dispatch: Dispatch) => void}
+ */
 export function clearError(): (dispatch: Dispatch) => void {
     return (dispatch: Dispatch) => {
         dispatch(clearErrorAction());
     };
 }
 
+/**
+ * Show error action type
+ */
 export interface IShowAppErrorAction extends IPayloadAction<string, IAppError> {
     type: ActionTypes.SHOW_ERROR;
 }
 
+/**
+ * Clear error action type
+ */
 export interface IClearErrorAction extends Action<string> {
     type: ActionTypes.CLEAR_ERROR;
 }
 
+/**
+ * Instance of show error action
+ * @type {(payload: IShowAppErrorAction["payload"]) => IPayloadAction<IShowAppErrorAction["type"], IShowAppErrorAction["payload"]>}
+ */
 export const showErrorAction = createPayloadAction<IShowAppErrorAction>(ActionTypes.SHOW_ERROR);
+
+/**
+ * Instance of clear error action
+ * @type {() => Action<IClearErrorAction["type"]>}
+ */
 export const clearErrorAction = createAction<IClearErrorAction>(ActionTypes.CLEAR_ERROR);
--- src/redux/reducers/appErrorReducer.ts
+++ src/redux/reducers/appErrorReducer.ts
@@ -2,13 +2,21 @@ import { ActionTypes } from "../actions/actionTypes";
 import { AnyAction } from "../actions/actionCreators";
 import { IAppError } from "../../models/applicationState";
 
+/**
+ * App Error Reducer
+ * Actions handled:
+ *  SHOW_ERROR
+ *  CLEAR_ERROR
+ * @param {IAppError} state
+ * @param {AnyAction} action
+ * @returns {any}
+ */
 export const reducer = (state: IAppError = null, action: AnyAction) => {
     switch (action.type) {
         case ActionTypes.SHOW_ERROR:
             return {...action.payload};
         case ActionTypes.CLEAR_ERROR:
             return null;
-        // Default
         default:
             return state;
     }
