bb78c260759e584bc49c7d48de01043708132e99
Wallace Breza <wallace.breza@microsoft.com>
2019-01-18 15:25:15 -0800

    Updating security token input component

--- src/react/components/common/securityTokenInput/securityTokenInput.tsx
+++ src/react/components/common/securityTokenInput/securityTokenInput.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { FieldProps } from "react-jsonschema-form";
+import { generateKey } from "../../../../common/crypto";
 
 export interface ISecurityTokenInputProps extends FieldProps {
 }
 
 export function SecurityTokenInput(props: ISecurityTokenInputProps) {
-    const { id, onChange, value } = props;
+    const inputRef = React.createRef<HTMLInputElement>();
+    let { id, onChange, value } = props;
+
+    if (!value) {
+        value = generateKey();
+        onChange(value);
+    }
+
+    function toggleInputType() {
+        const newType = inputRef.current.type === "password" ? "text" : "password";
+        inputRef.current.type = newType;
+    }
 
     return (
         <div className="input-group">
             <input id={id}
-                type="text"
+                ref={inputRef}
+                type="password"
                 readOnly={true}
                 className="form-control"
                 value={value}
                 onChange={(e) => onChange(e.target.value)} />
             <div className="input-group-append">
-                <button type="button" className="btn btn-primary">
+                <button type="button"
+                    className="btn btn-primary"
+                    onClick={toggleInputType}>
                     <i className="fas fa-unlock"></i>
                 </button>
                 <button type="button" className="btn btn-primary">
