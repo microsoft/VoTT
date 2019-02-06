df9d97b606bb26e7fb6b9066d000fb7559084e09
Tanner Barlow <tanner.barlow12@gmail.com>
2019-02-06 11:17:03 -0800

    Fix canvas helpers tests

--- src/react/components/pages/editorPage/canvasHelpers.test.ts
+++ src/react/components/pages/editorPage/canvasHelpers.test.ts
@@ -4,23 +4,26 @@ import MockFactory from "../../../../common/mockFactory";
 describe("Canvas Helpers", () => {
     it("Adds a tag to list", () => {
         const originalTags = MockFactory.createTestTags();
+        const originalLength = originalTags.length;
         const newTag = MockFactory.createTestTag("New Tag");
         const newTags = CanvasHelpers.toggleTag(
             originalTags,
             newTag,
         );
-        expect(newTags).toHaveLength(originalTags.length + 1);
+        expect(newTags).toHaveLength(originalLength + 1);
         expect(newTags[newTags.length - 1]).toEqual(newTag);
     });
 
     it("Removes a tag from list", () => {
         const originalTags = MockFactory.createTestTags();
+        const originalLength = originalTags.length;
+        const originalFirstTag = originalTags[0];
         const newTags = CanvasHelpers.toggleTag(
             originalTags,
             originalTags[0],
         );
-        expect(newTags).toHaveLength(originalTags.length - 1);
-        expect(newTags[0]).not.toEqual(originalTags[0]);
+        expect(newTags).toHaveLength(originalLength - 1);
+        expect(newTags[0]).not.toEqual(originalFirstTag);
     });
 
     it("Creates region data from region", () => {
