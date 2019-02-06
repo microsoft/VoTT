87d81a1ea4af70a922195cd2a468cca539664310
Tanner Barlow <tanner.barlow12@gmail.com>
2019-02-06 13:21:19 -0800

    Add more helper unit tests

--- src/react/components/pages/editorPage/canvasHelpers.test.ts
+++ src/react/components/pages/editorPage/canvasHelpers.test.ts
@@ -1,7 +1,8 @@
 import CanvasHelpers from "./canvasHelpers";
 import MockFactory from "../../../../common/mockFactory";
-import { RegionType } from "../../../../models/applicationState";
-import { RegionDataType } from "vott-ct/lib/js/CanvasTools/Core/RegionData";
+import { RegionType, IRegion } from "../../../../models/applicationState";
+import { RegionDataType, RegionData } from "vott-ct/lib/js/CanvasTools/Core/RegionData";
+import { Point2D } from "vott-ct/lib/js/CanvasTools/Core/Point2D";
 
 describe("Canvas Helpers", () => {
     it("Adds a tag to list", () => {
@@ -28,10 +29,50 @@ describe("Canvas Helpers", () => {
         expect(tags[0]).not.toEqual(originalFirstTag);
     });
 
+    it("Gets correct region data", () => {
+        const expected = new RegionData(
+            0, 0, 100, 100, 
+            [
+                new Point2D(0,0),
+                new Point2D(1, 0),
+                new Point2D(1,1),
+                new Point2D(0,0)
+            ],
+            RegionDataType.Polygon
+        );
+        
+        const region: IRegion = {
+            id: "test",
+            boundingBox: {
+                left: 0,
+                top: 0,
+                width: 100,
+                height: 100
+            },
+            points: [
+                {
+                    x: 0, y: 0
+                },
+                {
+                    x: 1, y: 0
+                },
+                {
+                    x: 1, y: 1
+                },{
+                    x: 0, y: 0
+                },
+            ],
+            type: RegionType.Polygon,
+            tags: []
+        }
+        expect(CanvasHelpers.getRegionData(region)).toEqual(expected);
+    });
+
     it("Gets correct region data type", () => {
         expect(CanvasHelpers.regionTypeToType(RegionType.Rectangle)).toEqual(RegionDataType.Rect);
         expect(CanvasHelpers.regionTypeToType(RegionType.Polygon)).toEqual(RegionDataType.Polygon);
         expect(CanvasHelpers.regionTypeToType(RegionType.Point)).toEqual(RegionDataType.Point);
         expect(CanvasHelpers.regionTypeToType(RegionType.Polyline)).toEqual(RegionDataType.Polyline);
+        expect(CanvasHelpers.regionTypeToType(null)).toBeUndefined();
     });
 });
