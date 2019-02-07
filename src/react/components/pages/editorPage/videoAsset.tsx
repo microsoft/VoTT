f79e74bd725beabfe0d0a95ce031f3542ea3b9ba
Wallace Breza <wallace.breza@microsoft.com>
2019-02-07 08:20:28 -0800

    Load and set child assets

--- src/react/components/pages/editorPage/videoAsset.tsx
+++ src/react/components/pages/editorPage/videoAsset.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
     Player, BigPlayButton, ControlBar, CurrentTimeDisplay,
     TimeDivider, PlaybackRateMenuButton, VolumeMenuButton,
@@ -9,6 +9,7 @@ import { AssetService } from "../../../../services/assetService";
 
 export interface IVideoAssetProps extends IAssetProps, React.Props<VideoAsset> {
     autoPlay?: boolean;
+    timestamp?: number;
     onChildAssetSelected?: (asset: IAsset) => void;
 }
 
@@ -26,7 +27,9 @@ export interface IVideoPlayerState {
 export class VideoAsset extends React.Component<IVideoAssetProps> {
     public static defaultProps: IVideoAssetProps = {
         autoPlay: true,
+        timestamp: 0,
         asset: null,
+        childAssets: [],
     };
 
     public state: IVideoAssetState = {
@@ -43,22 +46,31 @@ export class VideoAsset extends React.Component<IVideoAssetProps> {
         }
 
         return (
-            <Player ref={this.videoPlayer}
-                fluid={false}
-                width="100%"
-                height="100%"
-                autoPlay={autoPlay}
-                poster={""}
-                src={videoPath}
-            >
-                <BigPlayButton position="center" />
-                <ControlBar autoHide={false}>
-                    <CurrentTimeDisplay order={1.1} />
-                    <TimeDivider order={1.2} />
-                    <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.25]} order={7.1} />
-                    <VolumeMenuButton enabled order={7.2} />
-                </ControlBar>
-            </Player>
+            <Fragment>
+                <Player ref={this.videoPlayer}
+                    fluid={false}
+                    width="100%"
+                    height="100%"
+                    autoPlay={autoPlay}
+                    poster={""}
+                    src={videoPath}
+                >
+                    <BigPlayButton position="center" />
+                    <ControlBar autoHide={false}>
+                        <CurrentTimeDisplay order={1.1} />
+                        <TimeDivider order={1.2} />
+                        <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.25]} order={7.1} />
+                        <VolumeMenuButton enabled order={7.2} />
+                    </ControlBar>
+                </Player>
+                <div style={{ position: "absolute", top: 0, left: 0, width: "100%", zIndex: 2 }}>
+                    {this.props.childAssets.map((asset, i) => {
+                        return (
+                            <button key={asset.id} onClick={() => this.goToChildAsset(asset)}>{asset.timestamp}</button>
+                        );
+                    })}
+                </div>
+            </Fragment >
         );
     }
 
@@ -70,6 +82,21 @@ export class VideoAsset extends React.Component<IVideoAssetProps> {
         if (this.props.asset !== prevProps.asset) {
             this.setState({ loaded: false });
         }
+        if (this.props.timestamp !== prevProps.timestamp) {
+            this.seekToTimestamp();
+        }
+    }
+
+    private seekToTimestamp = () => {
+        if (this.props.timestamp > 0) {
+            this.videoPlayer.current.pause();
+            this.videoPlayer.current.seek(this.props.timestamp);
+        }
+    }
+
+    private goToChildAsset(asset: IAsset) {
+        this.videoPlayer.current.pause();
+        this.videoPlayer.current.seek(asset.timestamp);
     }
 
     private onVideoStateChange = (state: Readonly<IVideoPlayerState>, prev: Readonly<IVideoPlayerState>) => {
@@ -77,6 +104,7 @@ export class VideoAsset extends React.Component<IVideoAssetProps> {
             // Video initial load complete
             this.raiseLoaded();
             this.raiseActivated();
+            this.seekToTimestamp();
         } else if (state.paused && (state.currentTime !== prev.currentTime || state.seeking !== prev.seeking)) {
             // Video is paused
             this.raiseChildAssetSelected(state);
