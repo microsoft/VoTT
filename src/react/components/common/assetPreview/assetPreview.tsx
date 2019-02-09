3822b2d20484cc89dfba1ad24efe84a7e922d77b
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-02-08 19:42:40 -0800

    Refactored based on last changes and removed duplicates

--- src/react/components/common/assetPreview/assetPreview.tsx
+++ src/react/components/common/assetPreview/assetPreview.tsx
@@ -3,6 +3,7 @@ import { IAsset, AssetType } from "../../../../models/applicationState";
 import { strings } from "../../../../common/strings";
 import { ImageAsset } from "./imageAsset";
 import { VideoAsset } from "./videoAsset";
+import { TFRecordAsset } from "./tfrecordAsset";
 
 export type ContentSource = HTMLImageElement | HTMLVideoElement;
 
@@ -71,6 +72,12 @@ export class AssetPreview extends React.Component<IAssetPreviewProps, IAssetPrev
                         autoPlay={autoPlay}
                         onLoaded={this.onAssetLoad}
                         onChildAssetSelected={this.props.onChildAssetSelected}
+                        onActivated={this.props.onActivated}
+                        onDeactivated={this.props.onDeactivated} />
+                }
+                {asset.type === AssetType.TFRecord &&
+                    <TFRecordAsset asset={asset}
+                        onLoaded={this.onAssetLoad}
                         onActivated={this.props.onActivated}
                         onDeactivated={this.props.onDeactivated} />
                 }
--- src/react/components/common/assetPreview/tfrecordAsset.test.tsx
+++ src/react/components/common/assetPreview/tfrecordAsset.test.tsx
@@ -1,26 +1,47 @@
 import React from "react";
 import { IAssetProps } from "./assetPreview";
 import { ReactWrapper, mount } from "enzyme";
-import { ImageAsset } from "./imageAsset";
-import MockFactory from "../../../common/mockFactory";
+import { TFRecordAsset } from "./tfrecordAsset";
+import MockFactory from "../../../../common/mockFactory";
+import { TFRecordsBuilder, FeatureType } from "../../../../providers/export/tensorFlowRecords/tensorFlowBuilder";
+import HtmlFileReader from "../../../../common/htmlFileReader";
 
 describe("Image Asset Component", () => {
     // tslint:disable-next-line:max-line-length
+    const dataImage64 = "R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7";
+    const dataImage = new Uint8Array(Buffer.from(dataImage64, "base64"));
+
+    // tslint:disable-next-line:max-line-length
     const dataUri = "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7";
+
+    let tfrecords: Buffer;
+    beforeEach(() => {
+        let builder: TFRecordsBuilder;
+        builder = new TFRecordsBuilder();
+        builder.addFeature("image/encoded", FeatureType.Binary, dataImage);
+
+        const buffer = builder.build();
+        tfrecords = TFRecordsBuilder.buildTFRecords([buffer]);
+    });
+
+    HtmlFileReader.getAssetArray = jest.fn((asset) => {
+        return Promise.resolve<Uint8Array>(new Uint8Array(tfrecords));
+    });
+
     let wrapper: ReactWrapper<IAssetProps> = null;
     const onLoadHandler = jest.fn();
     MockFactory.createTestAsset("test");
     const defaultProps: IAssetProps = {
         asset: {
             ...MockFactory.createTestAsset("test"),
-            path: dataUri,
+            path: "abc",
         },
         onLoaded: onLoadHandler,
     };
 
     function createComponent(props?: IAssetProps): ReactWrapper<IAssetProps> {
         props = props || defaultProps;
-        return mount(<ImageAsset {...props} />);
+        return mount(<TFRecordAsset {...props} />);
     }
 
     it("renders landscape image correctly", () => {
@@ -36,7 +57,7 @@ describe("Image Asset Component", () => {
         expect(img.exists()).toBe(true);
         expect(img.props()).toEqual(expect.objectContaining({
             className: "landscape",
-            src: defaultProps.asset.path,
+            src: dataUri,
         }));
     });
 
@@ -53,7 +74,7 @@ describe("Image Asset Component", () => {
         expect(img.exists()).toBe(true);
         expect(img.props()).toEqual(expect.objectContaining({
             className: "portrait",
-            src: defaultProps.asset.path,
+            src: dataUri,
         }));
     });
 
