305b6fbadd5f7961036c297ddfb85439820af92f
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-02-12 15:32:35 -0800

    Removed duplicated mock

--- src/common/htmlFileReader.test.ts
+++ src/common/htmlFileReader.test.ts
@@ -123,16 +123,6 @@ describe("Html File Reader", () => {
 
         it("Downloads a byte array from the asset path", async () => {
             const asset = AssetService.createAssetFromFilePath("https://server.com/image.jpg");
-            axios.get = jest.fn((url, config) => {
-                return Promise.resolve<AxiosResponse>({
-                    config,
-                    headers: null,
-                    status: 200,
-                    statusText: "OK",
-                    data: [1, 2, 3],
-                });
-            });
-
             const result = await HtmlFileReader.getAssetArray(asset);
             expect(result).not.toBeNull();
             expect(result).toBeInstanceOf(Uint8Array);
