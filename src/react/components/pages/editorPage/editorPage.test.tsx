e859c1fbbe9bffaf4f840a364d3bed5270e41ef3
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-02-12 18:39:02 -0800

    temporary comment some code

--- src/react/components/pages/editorPage/editorPage.test.tsx
+++ src/react/components/pages/editorPage/editorPage.test.tsx
@@ -86,7 +86,7 @@ describe("Editor Page Component", () => {
         const editorMock = Editor as any;
         editorMock.prototype.addContentSource = jest.fn(() => Promise.resolve());
         editorMock.prototype.scaleRegionToSourceSize = jest.fn((regionData: any) => regionData);
-        editorMock.prototype.RM = new RegionsManager(null, null);
+        // JACOPO: editorMock.prototype.RM = new RegionsManager(null, null);
         editorMock.prototype.AS = {setSelectionMode: jest.fn()};
     });
 
