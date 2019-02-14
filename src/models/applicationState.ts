215bbd937b5b2dd5fef911ab1dd522d551e8a1f4
Elizabeth Halper <elhalper@microsoft.com>
2019-02-14 11:42:27 -0800

    fixed documentation

--- src/models/applicationState.ts
+++ src/models/applicationState.ts
@@ -99,7 +99,7 @@ export interface IAppSettings {
 export interface IProject {
     id: string;
     name: string;
-    version?: string;
+    version: string;
     securityToken: string;
     description?: string;
     tags: ITag[];
