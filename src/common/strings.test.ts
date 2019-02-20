aea056ea267ebaadb20b089b4a7b15bc55b79d70
Luisa de Miranda <lemiranda@gmail.com>
2019-02-20 07:27:50 -0800

    rebasing and squashing wabrez/integrat-vott-react (#591)
    * rebasing and squashing wabrez/integrat-vott-react
    
    * updated canvas tools

--- src/common/strings.test.ts
+++ src/common/strings.test.ts
@@ -93,80 +93,49 @@ describe("Localization tests", () => {
             }
         });
 
-        it("Tag Editor Modal Form", () => {
-            for (const language of languages) {
-                const languageJson = getLanguageJson(language);
-                // tslint:disable-next-line:no-var-requires
-                const formJson = require("../react/components/common/tagsInput/tagEditorModal/tagEditorModal.json");
-                strings.setLanguage(language);
-                const newFormJson = addLocValues(formJson);
-                const formProps = newFormJson.properties;
-                const common = languageJson.common;
+        it("Interpolate processing string template correctly", () => {
+            const template = "Hello ${user.name}, my name is ${bot.handle}";
+            const params = {
+                user: {
+                    name: "John Doe",
+                },
+                bot: {
+                    handle: "VoTT bot",
+                },
+            };
 
-                const lTags = languageJson.tags;
-
-                expect(formProps.name.title).toEqual(lTags.modal.name);
-                expect(formProps.color.title).toEqual(lTags.modal.color);
-                expect(formProps.color.enumNames[0]).toEqual(lTags.colors.white);
-                expect(formProps.color.enumNames[1]).toEqual(lTags.colors.gray);
-                expect(formProps.color.enumNames[2]).toEqual(lTags.colors.red);
-                expect(formProps.color.enumNames[3]).toEqual(lTags.colors.maroon);
-                expect(formProps.color.enumNames[4]).toEqual(lTags.colors.yellow);
-                expect(formProps.color.enumNames[5]).toEqual(lTags.colors.olive);
-                expect(formProps.color.enumNames[6]).toEqual(lTags.colors.lime);
-                expect(formProps.color.enumNames[7]).toEqual(lTags.colors.green);
-                expect(formProps.color.enumNames[8]).toEqual(lTags.colors.aqua);
-                expect(formProps.color.enumNames[9]).toEqual(lTags.colors.teal);
-                expect(formProps.color.enumNames[10]).toEqual(lTags.colors.blue);
-                expect(formProps.color.enumNames[11]).toEqual(lTags.colors.navy);
-                expect(formProps.color.enumNames[12]).toEqual(lTags.colors.fuschia);
-                expect(formProps.color.enumNames[13]).toEqual(lTags.colors.purple);
-            }
+            const result = interpolate(template, params);
+            expect(result).toEqual(`Hello ${params.user.name}, my name is ${params.bot.handle}`);
         });
-    });
 
-    it("Interpolate processing string template correctly", () => {
-        const template = "Hello ${user.name}, my name is ${bot.handle}";
-        const params = {
-            user: {
-                name: "John Doe",
-            },
-            bot: {
-                handle: "VoTT bot",
-            },
-        };
+        it("Interpolate processes a JSON object template correctly", () => {
+            const template = {
+                user: {
+                    firstName: "${user.firstName}",
+                    lastName: "${user.lastName}",
+                },
+                address: {
+                    street: "${address.street}",
+                    city: "${address.city}",
+                    state: "${address.state}",
+                    zipCode: "${address.zipCode}",
+                },
+            };
+            const params = {
+                user: {
+                    firstName: "John",
+                    lastName: "Doe",
+                },
+                address: {
+                    street: "1 Microsoft Way",
+                    city: "Redmond",
+                    state: "WA",
+                    zipCode: "98052",
+                },
+            };
 
-        const result = interpolate(template, params);
-        expect(result).toEqual(`Hello ${params.user.name}, my name is ${params.bot.handle}`);
-    });
-
-    it("Interpolate processes a JSON object template correctly", () => {
-        const template = {
-            user: {
-                firstName: "${user.firstName}",
-                lastName: "${user.lastName}",
-            },
-            address: {
-                street: "${address.street}",
-                city: "${address.city}",
-                state: "${address.state}",
-                zipCode: "${address.zipCode}",
-            },
-        };
-        const params = {
-            user: {
-                firstName: "John",
-                lastName: "Doe",
-            },
-            address: {
-                street: "1 Microsoft Way",
-                city: "Redmond",
-                state: "WA",
-                zipCode: "98052",
-            },
-        };
-
-        const result = interpolateJson(template, params);
-        expect(result).toEqual(params);
+            const result = interpolateJson(template, params);
+            expect(result).toEqual(params);
+        });
     });
 });
