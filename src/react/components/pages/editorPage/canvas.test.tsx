1f78bdf6217cfe11d218a9f891e4e31449fc80e8
Wallace Breza <wallace@breza.me>
2019-03-12 18:07:33 -0700

    fix: Re-apply tags in editor when project tags are updated (#673)
    Resolves issue where if tag color is modified while in the editor page, the color changes are not applied till you reload or navigate away from the current asset
    
    Resolves AB#17264

--- src/react/components/pages/editorPage/canvas.test.tsx
+++ src/react/components/pages/editorPage/canvas.test.tsx
@@ -18,7 +18,6 @@ import { Rect } from "vott-ct/lib/js/CanvasTools/Core/Rect";
 import { SelectionMode } from "vott-ct/lib/js/CanvasTools/Interface/ISelectorSettings";
 
 describe("Editor Canvas", () => {
-
     function createComponent(canvasProps?: ICanvasProps, assetPreviewProps?: IAssetPreviewProps)
         : ReactWrapper<ICanvasProps, ICanvasState, Canvas> {
         const props = createProps();
@@ -76,8 +75,9 @@ describe("Editor Canvas", () => {
         editorMock.prototype.scaleRegionToSourceSize = jest.fn((regionData: any) => regionData);
         editorMock.prototype.RM = new RegionsManager(null, null);
         editorMock.prototype.AS = {
-            setSelectionMode: jest.fn(({mode, template = null}) => {selectionMode = {mode, template}; }),
-            getSelectorSettings: jest.fn(() => selectionMode) };
+            setSelectionMode: jest.fn(({ mode, template = null }) => { selectionMode = { mode, template }; }),
+            getSelectorSettings: jest.fn(() => selectionMode),
+        };
 
         const clipboard = (navigator as any).clipboard;
         if (!(clipboard && clipboard.writeText)) {
@@ -506,7 +506,7 @@ describe("Editor Canvas", () => {
     });
 
     it("Copies currently selected regions to clipboard", () => {
-        const wrapper = createComponent().find(Canvas);
+        const wrapper = createComponent();
         const canvas = wrapper.instance() as Canvas;
         mockSelectedRegions(["test1"]);
 
@@ -529,7 +529,7 @@ describe("Editor Canvas", () => {
                 ...cProps.selectedAsset,
                 regions: [copiedRegion],
             },
-        }).find(Canvas);
+        });
 
         const canvas = wrapper.instance() as Canvas;
 
@@ -563,7 +563,7 @@ describe("Editor Canvas", () => {
     });
 
     it("Cuts currently selected regions to clipboard", async () => {
-        const wrapper = createComponent().find(Canvas);
+        const wrapper = createComponent();
         const original: IAssetMetadata = {
             ...wrapper.prop("selectedAsset"),
         };
@@ -585,11 +585,26 @@ describe("Editor Canvas", () => {
     });
 
     it("Clears all regions from asset", async () => {
-        const wrapper = createComponent().find(Canvas);
+        const wrapper = createComponent();
         const clearConfirm = wrapper.find(Confirm) as ReactWrapper<IConfirmProps>;
         clearConfirm.props().onConfirm();
 
         await MockFactory.flushUi();
         expect(wrapper.state().currentAsset.regions).toEqual([]);
+    });
+
+    it("Tags are re-applied when the project tags are updated", () => {
+        const wrapper = createComponent();
+        const assetMetadata = wrapper.props().selectedAsset;
+        const project = { ...wrapper.props().project };
+        const tags = [...project.tags];
+        tags[0].color = "#FFCCFF";
+        project.tags = tags;
+
+        const editor = wrapper.instance().editor;
+        const updateTagsSpy = jest.spyOn(editor.RM, "updateTagsById");
+
+        wrapper.setProps({ project });
+        expect(updateTagsSpy).toBeCalledTimes(assetMetadata.regions.length);
     });
 });
