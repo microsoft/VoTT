2ee9a5ef227d8c4e9501554cf8d781bf8de67536
My Ho <mydiemho@gmail.com>
2019-03-13 11:20:23 -0700

    refactor: remove redux-invariant & redux-logging from production
    these middlewares are intend to be use only during development.

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -2,4 +2,8 @@ export class Env {
     public static get() {
         return process.env.NODE_ENV;
     }
+
+    public static isDevelopment(): boolean {
+        return this.get() !== "production";
+    }
 }
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,13 +1,14 @@
 import { applyMiddleware, createStore, Store } from "redux";
-import reduxImmutableStateInvarient from "redux-immutable-state-invariant";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 import { IApplicationState } from "../../models/applicationState";
 import { createLocalStorage, mergeInitialState } from "../middleware/localStorage";
+import { Env } from "../../common/environment";
 
 /**
- * Creates initial redux store from initial aplication state
+ * Creates initial redux store from initial application state
  * @param initialState - Initial state of application
  * @param useLocalStorage - Whether or not to use localStorage middleware
  */
@@ -15,13 +16,22 @@ export default function createReduxStore(
     initialState?: IApplicationState,
     useLocalStorage: boolean = false): Store {
     const paths: string[] = ["appSettings", "connections", "recentProjects"];
-    const middlewares = useLocalStorage
-        ? applyMiddleware(thunk, reduxImmutableStateInvarient(), createLogger(), createLocalStorage({ paths }))
-        : applyMiddleware(thunk, reduxImmutableStateInvarient(), createLogger());
+
+    const middlewares = [];
+
+    if (useLocalStorage) {
+        middlewares.push(createLocalStorage({paths}));
+    }
+
+    if (Env.isDevelopment()) {
+        middlewares.push(reduxImmutableStateInvariant(), createLogger());
+    }
+
+    middlewares.push(thunk);
 
     return createStore(
         rootReducer,
         useLocalStorage ? mergeInitialState(initialState, paths) : initialState,
-        middlewares,
+        applyMiddleware(...middlewares),
     );
 }
