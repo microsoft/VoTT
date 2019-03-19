cd01a4ac1d0524b57b8ccf769c7d984470f557b2
My <mydiemho@users.noreply.github.com>
2019-03-19 13:05:38 -0700

    refactor: remove redux-invariant & redux-logging from production (#680)
    * these middlewares are intend to be use only during development.
    * also move a few packages from devDependencies to dependencies

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,13 +1,12 @@
 import { applyMiddleware, createStore, Store } from "redux";
-import reduxImmutableStateInvarient from "redux-immutable-state-invariant";
-import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 import { IApplicationState } from "../../models/applicationState";
-import { createLocalStorage, mergeInitialState } from "../middleware/localStorage";
+import { mergeInitialState } from "../middleware/localStorage";
+import { Env } from "../../common/environment";
 
 /**
- * Creates initial redux store from initial aplication state
+ * Creates initial redux store from initial application state
  * @param initialState - Initial state of application
  * @param useLocalStorage - Whether or not to use localStorage middleware
  */
@@ -15,13 +14,31 @@ export default function createReduxStore(
     initialState?: IApplicationState,
     useLocalStorage: boolean = false): Store {
     const paths: string[] = ["appSettings", "connections", "recentProjects"];
-    const middlewares = useLocalStorage
-        ? applyMiddleware(thunk, reduxImmutableStateInvarient(), createLogger(), createLocalStorage({ paths }))
-        : applyMiddleware(thunk, reduxImmutableStateInvarient(), createLogger());
+
+    let middlewares = [thunk];
+
+    if (useLocalStorage) {
+        const localStorage = require("../middleware/localStorage");
+        const storage = localStorage.createLocalStorage({paths});
+        middlewares = [
+            ...middlewares,
+            storage,
+        ];
+    }
+
+    if (Env.get() === "development") {
+        const logger = require("redux-logger");
+        const reduxImmutableStateInvariant = require("redux-immutable-state-invariant");
+        middlewares = [
+            ...middlewares,
+            reduxImmutableStateInvariant.default(),
+            logger.createLogger(),
+        ];
+    }
 
     return createStore(
         rootReducer,
         useLocalStorage ? mergeInitialState(initialState, paths) : initialState,
-        middlewares,
+        applyMiddleware(...middlewares),
     );
 }
