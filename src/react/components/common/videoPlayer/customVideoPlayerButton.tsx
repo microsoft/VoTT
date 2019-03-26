c632c0905e937c43ff8f941d9cef8a7a7f55dec6
Tanner Barlow <tanner.barlow12@gmail.com>
2019-03-26 14:41:44 -0700

    fix help menu

--- src/react/components/common/videoPlayer/customVideoPlayerButton.tsx
+++ src/react/components/common/videoPlayer/customVideoPlayerButton.tsx
@@ -20,7 +20,8 @@ export class CustomVideoPlayerButton extends React.Component<ICustomVideoPlayerB
                     <KeyboardBinding keyEventType={KeyEventType.KeyDown}
                         displayName={this.props.tooltip}
                         accelerators={this.props.accelerators}
-                        handler={this.props.onClick} />
+                        handler={this.props.onClick} 
+                        icon={this.props.icon}/>
                 }
                 <button
                     type="button"
