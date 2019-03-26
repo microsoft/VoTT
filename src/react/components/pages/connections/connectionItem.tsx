b6d89e00f3ce7b38406bd238ef1f9c06447505b2
Tanner Barlow <tanner.barlow12@gmail.com>
2019-03-26 14:15:13 -0700

    Revert changes to connection items

--- src/react/components/pages/connections/connectionItem.tsx
+++ src/react/components/pages/connections/connectionItem.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function ConnectionItem({ id, name, onClick, onDelete }) {
+export default function ConnectionItem({ item, onClick, onDelete }) {
     return (
         <li onClick={onClick}>
-            <NavLink to={`/connections/${id}`}>
+            <NavLink to={`/connections/${item.id}`}>
                 <i className="fas fa-edit"></i>
-                <span className="px-2">{name}</span>
+                <span className="px-2">{item.name}</span>
                 <div className="float-right delete-btn" onClick={onDelete}><i className="fas fa-trash"></i></div>
             </NavLink>
         </li>
