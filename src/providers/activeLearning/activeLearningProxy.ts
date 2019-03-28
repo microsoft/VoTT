32baaef9a1de012016f924c51b2b25f6d8e74414
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-03-28 15:26:34 -0700

    WIP adding Active Learning provider proxy

---
+++ src/providers/activeLearning/activeLearningProxy.ts
@@ -0,0 +1,41 @@
+import { IpcRendererProxy } from "../../common/ipcRendererProxy";
+import { IAsset, StorageType } from "../../models/applicationState";
+
+const PROXY_NAME = "LocalActiveLearningDomain";
+
+/**
+ * Interface for VoTT Active Learning Proxy
+ * @member testMethod - Test Method
+ */
+export interface IActiveLearningProvider {
+    testMethod(input: string): Promise<string>;
+}
+
+/**
+ * Options for Local Active Learning
+ * @member folderPath - Path to folder being used in provider
+ */
+export interface ILocalActiveLearningProxyOptions {
+    folderPath: string;
+}
+
+/**
+ * Active Learning Provider for Local File System. Only available in Electron application
+ * Leverages the IpcRendererProxy
+ */
+export class LocalActiveLearningProxy implements IActiveLearningProvider {
+    constructor(private options?: ILocalActiveLearningProxyOptions) {
+        if (!this.options) {
+            this.options = {
+                folderPath: null,
+            };
+        }
+    }
+
+    /**
+     * Test Remote API
+     */
+    public testMethod(input: string): Promise<string> {
+        return IpcRendererProxy.send(`${PROXY_NAME}:testMethod`, [input]);
+    }
+}
