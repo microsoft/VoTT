e427e277a197fa41c320d916e402698bca0b8f53
Wallace Breza <wallace@breza.me>
2019-04-06 12:00:07 -0700

    test: Verify includeImages property in Json export provider

--- src/providers/export/vottJson.test.ts
+++ src/providers/export/vottJson.test.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { VottJsonExportProvider } from "./vottJson";
+import { VottJsonExportProvider, IVottJsonExportProviderOptions } from "./vottJson";
 import registerProviders from "../../registerProviders";
 import { ExportAssetState } from "./exportProvider";
 import { ExportProviderFactory } from "./exportProviderFactory";
@@ -60,8 +60,9 @@ describe("VoTT Json Export Provider", () => {
     });
 
     it("Can be instantiated through the factory", () => {
-        const options: IExportProviderOptions = {
+        const options: IVottJsonExportProviderOptions = {
             assetState: ExportAssetState.All,
+            includeImages: true,
         };
         const exportProvider = ExportProviderFactory.create("vottJson", testProject, options);
         expect(exportProvider).not.toBeNull();
@@ -82,12 +83,15 @@ describe("VoTT Json Export Provider", () => {
             });
 
             const storageProviderMock = LocalFileSystemProxy as jest.Mock<LocalFileSystemProxy>;
+            storageProviderMock.prototype.writeText.mockClear();
+            storageProviderMock.prototype.writeBinary.mockClear();
             storageProviderMock.mockClear();
         });
 
         it("Exports all assets", async () => {
-            const options: IExportProviderOptions = {
+            const options: IVottJsonExportProviderOptions = {
                 assetState: ExportAssetState.All,
+                includeImages: true,
             };
 
             const exportProvider = new VottJsonExportProvider(testProject, options);
@@ -95,18 +99,25 @@ describe("VoTT Json Export Provider", () => {
 
             const storageProviderMock = LocalFileSystemProxy as any;
             const exportJson = storageProviderMock.mock.instances[0].writeText.mock.calls[0][1];
-            const exportObject = JSON.parse(exportJson);
+            const exportObject = JSON.parse(exportJson) as IProject;
 
             const exportedAssets = _.values(exportObject.assets);
 
+            // Ensure provider information not included in export JSON
+            expect(exportObject.sourceConnection).toBeUndefined();
+            expect(exportObject.targetConnection).toBeUndefined();
+            expect(exportObject.exportFormat).toBeUndefined();
+
+            // Verify exported assets match expectations
             expect(exportedAssets.length).toEqual(testAssets.length);
             expect(LocalFileSystemProxy.prototype.writeText)
                 .toBeCalledWith(expectedFileName, expect.any(String));
         });
 
         it("Exports only visited assets (includes tagged)", async () => {
-            const options: IExportProviderOptions = {
+            const options: IVottJsonExportProviderOptions = {
                 assetState: ExportAssetState.Visited,
+                includeImages: true,
             };
 
             const exportProvider = new VottJsonExportProvider(testProject, options);
@@ -120,14 +131,21 @@ describe("VoTT Json Export Provider", () => {
             const expectedAssets = _.values(testProject.assets)
                 .filter((asset) => asset.state === AssetState.Visited || asset.state === AssetState.Tagged);
 
+            // Ensure provider information not included in export JSON
+            expect(exportObject.sourceConnection).toBeUndefined();
+            expect(exportObject.targetConnection).toBeUndefined();
+            expect(exportObject.exportFormat).toBeUndefined();
+
+            // Verify exported assets match expectations
             expect(exportedAssets.length).toEqual(expectedAssets.length);
             expect(LocalFileSystemProxy.prototype.writeText)
                 .toBeCalledWith(expectedFileName, expect.any(String));
         });
 
         it("Exports only tagged assets", async () => {
-            const options: IExportProviderOptions = {
+            const options: IVottJsonExportProviderOptions = {
                 assetState: ExportAssetState.Tagged,
+                includeImages: true,
             };
 
             const exportProvider = new VottJsonExportProvider(testProject, options);
@@ -140,9 +158,41 @@ describe("VoTT Json Export Provider", () => {
             const exportedAssets = _.values(exportObject.assets);
             const expectedAssets = _.values(testProject.assets).filter((asset) => asset.state === AssetState.Tagged);
 
+            // Ensure provider information not included in export JSON
+            expect(exportObject.sourceConnection).toBeUndefined();
+            expect(exportObject.targetConnection).toBeUndefined();
+            expect(exportObject.exportFormat).toBeUndefined();
+
+            // Verify exported assets match expectations
             expect(exportedAssets.length).toEqual(expectedAssets.length);
             expect(LocalFileSystemProxy.prototype.writeText)
                 .toBeCalledWith(expectedFileName, expect.any(String));
+        });
+
+        it("Includes images in export when option is selected", async () => {
+            const options: IVottJsonExportProviderOptions = {
+                assetState: ExportAssetState.All,
+                includeImages: true,
+            };
+
+            const exportProvider = new VottJsonExportProvider(testProject, options);
+            await exportProvider.export();
+
+            expect(LocalFileSystemProxy.prototype.writeText).toBeCalledTimes(1);
+            expect(LocalFileSystemProxy.prototype.writeBinary).toBeCalledTimes(testAssets.length);
+        });
+
+        it("Does not include images in export when option is not selected", async () => {
+            const options: IVottJsonExportProviderOptions = {
+                assetState: ExportAssetState.All,
+                includeImages: false,
+            };
+
+            const exportProvider = new VottJsonExportProvider(testProject, options);
+            await exportProvider.export();
+
+            expect(LocalFileSystemProxy.prototype.writeText).toBeCalledTimes(1);
+            expect(LocalFileSystemProxy.prototype.writeBinary).toBeCalledTimes(0);
         });
     });
 });
