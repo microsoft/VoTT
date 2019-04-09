04e1af360b99024639f356030ea37964c749b2aa
P.J. Little <peter.jacob.little@gmail.com>
2019-04-09 16:56:58 -0700

    fix: CORS warning message for Bing Image Search (#732)
    Error message displays when adding a Bing Image Search asset provider
    type. This is only applicable for the Browser target.
    
    AB#17153

--- src/react/components/pages/connections/connectionForm.test.tsx
+++ src/react/components/pages/connections/connectionForm.test.tsx
@@ -3,6 +3,11 @@ import { mount, ReactWrapper } from "enzyme";
 import MockFactory from "../../../../common/mockFactory";
 import ConnectionForm, { IConnectionFormProps, IConnectionFormState } from "./connectionForm";
 
+jest.mock("../../../../common/hostProcess", () => ({
+    isBrowser: jest.fn(),
+}));
+import { isBrowser } from "../../../../common/hostProcess";
+
 describe("Connection Form", () => {
     const onSubmitHandler = jest.fn();
     const testConnection = MockFactory.createTestConnection("test", "localFileSystemProxy");
@@ -45,6 +50,38 @@ describe("Connection Form", () => {
         expect("apiKey" in providerOptions).toBe(true);
         expect("query" in providerOptions).toBe(true);
         expect("aspectRatio" in providerOptions).toBe(true);
+    });
+
+    it("should display warning for Bing Image Search when running in a browser", async () => {
+        const isBrowserMock = isBrowser as jest.Mock;
+        isBrowserMock.mockReturnValue(true);
+
+        wrapper
+            .find("select#root_providerType")
+            .simulate("change", { target: { value: "bingImageSearch" } });
+
+        await MockFactory.flushUi();
+        wrapper.update();
+
+        const providerOptions = wrapper.state().formData.providerOptions;
+        expect(wrapper.state().formData.providerType).toEqual("bingImageSearch");
+        expect(wrapper.exists("div.alert")).toEqual(true);
+    });
+
+    it("should display warning for Blob Storage when running in a browser", async () => {
+        const isBrowserMock = isBrowser as jest.Mock;
+        isBrowserMock.mockReturnValue(true);
+
+        wrapper
+            .find("select#root_providerType")
+            .simulate("change", { target: { value: "azureBlobStorage" } });
+
+        await MockFactory.flushUi();
+        wrapper.update();
+
+        const providerOptions = wrapper.state().formData.providerOptions;
+        expect(wrapper.state().formData.providerType).toEqual("azureBlobStorage");
+        expect(wrapper.exists("div.alert")).toEqual(true);
     });
 
     it("should call the onSubmit event handler when the form is submitted", async () => {
