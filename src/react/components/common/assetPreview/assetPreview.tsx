896a9757596d2f8dfdd3d0d3a7145dd7abb46355
Wallace Breza <wallace@breza.me>
2019-04-10 16:07:26 -0700

    feat: Enforce asset tagging before switching assets (#730)
    Enforces that there are no untagged regions within an asset before navigating away from the current loaded asset. Displays an alert warning when user attempt to navigate.

--- src/react/components/common/assetPreview/assetPreview.tsx
+++ src/react/components/common/assetPreview/assetPreview.tsx
@@ -25,6 +25,8 @@ export interface IAssetProps {
     childAssets?: IAsset[];
     /** Additional settings for this asset */
     additionalSettings?: IAssetPreviewSettings;
+    /** Specifies whether the asset controls are enabled */
+    controlsEnabled?: boolean;
     /** Event handler that fires when the asset has been loaded */
     onLoaded?: (ContentSource: ContentSource) => void;
     /** Event handler that fires when the asset has been activated (ex. Video resumes playing) */
@@ -37,6 +39,8 @@ export interface IAssetProps {
     onError?: (event: React.SyntheticEvent) => void;
     /** Event handler that fires when the loaded asset has changed */
     onAssetChanged?: (asset: IAsset) => void;
+    /** Event handler that fires right before an asset has changed */
+    onBeforeAssetChanged?: () => boolean;
 }
 
 /**
@@ -74,6 +78,7 @@ export class AssetPreview extends React.Component<IAssetPreviewProps, IAssetPrev
         asset: null,
         childAssets: [],
         autoPlay: false,
+        controlsEnabled: true,
     };
 
     /** The internal state for the component */
@@ -146,12 +151,14 @@ export class AssetPreview extends React.Component<IAssetPreviewProps, IAssetPrev
             case AssetType.Video:
             case AssetType.VideoFrame:
                 return <VideoAsset asset={rootAsset}
+                    controlsEnabled={this.props.controlsEnabled}
                     additionalSettings={this.props.additionalSettings}
                     childAssets={childAssets}
                     timestamp={asset.timestamp}
                     autoPlay={autoPlay}
                     onLoaded={this.onAssetLoad}
                     onError={this.onError}
+                    onBeforeAssetChanged={this.props.onBeforeAssetChanged}
                     onChildAssetSelected={this.onChildAssetSelected}
                     onActivated={this.props.onActivated}
                     onDeactivated={this.props.onDeactivated} />;
@@ -192,6 +199,12 @@ export class AssetPreview extends React.Component<IAssetPreviewProps, IAssetPrev
     }
 
     private onChildAssetSelected = (asset: IAsset) => {
+        if (this.props.onBeforeAssetChanged) {
+            if (!this.props.onBeforeAssetChanged()) {
+                return;
+            }
+        }
+
         if (this.props.onChildAssetSelected) {
             this.props.onChildAssetSelected(asset);
         }
