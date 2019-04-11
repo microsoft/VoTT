fb093b8d46ef9beb2a2acd4d96b7f9e209a7b360
Wallace Breza <wallace@breza.me>
2019-04-11 14:13:13 -0700

    fix: Excludes meta key from keyboard manager

--- src/react/components/common/keyboardManager/keyboardManager.tsx
+++ src/react/components/common/keyboardManager/keyboardManager.tsx
@@ -23,7 +23,7 @@ export class KeyboardManager extends React.Component<any, IKeyboardContext> {
         keyboard: new KeyboardRegistrationManager(),
     };
 
-    private nonSupportedKeys = new Set(["Ctrl", " Control", "Alt"]);
+    private nonSupportedKeys = new Set(["Meta", "Ctrl", " Control", "Alt"]);
     private inputElementTypes = new Set(["input", "select", "textarea"]);
 
     public componentDidMount() {
