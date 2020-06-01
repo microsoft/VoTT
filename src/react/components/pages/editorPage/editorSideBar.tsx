211f32d9b4d8d132531e77a22d4fee8548801b66
Vott <vott@microsoft.com>
2020-06-01 10:44:55 -0700

    fix: sidebar not showing thumbnails

--- src/react/components/pages/editorPage/editorSideBar.tsx
+++ src/react/components/pages/editorPage/editorSideBar.tsx
@@ -42,7 +42,7 @@ export default class EditorSideBar extends React.Component<IEditorSideBarProps, 
 
     public render() {
         return (
-            <div className="editor-page-bottombar-nav">
+            <div className="editor-page-sidebar-nav">
                 <AutoSizer>
                     {({ height, width }) => (
                         <List
